870da862c1f82a2b50572ab894326544bb6debaf
cr0ss4ire <cowfish50@yahoo.com>
2021-11-04 18:26:13 +0800

    modified:   ps_web/src/components/task/Index.vue

--- ps_web/src/components/task/Index.vue
+++ ps_web/src/components/task/Index.vue
@@ -4,9 +4,9 @@
             <el-col :span="19">
                 <el-form :inline="true" :model="plugin_query">
                     <el-form-item>
-                        <el-input v-model="plugin_query.name" clearable placeholder="漏洞插件名称"></el-input>
+                        <el-input v-model="plugin_query.name" clearable placeholder="任务名称"></el-input>
                     </el-form-item>
-                    <el-form-item style="width:12%">
+                    <!--<el-form-item style="width:12%">
                     <el-select v-model="plugin_query.vul_type_id" @change="search_by_vultype()" clearable placeholder="漏洞类型">
                         <el-option v-for="item in vultype_options" :label="item.name" :key="item.id" :value="item.id"></el-option>
                     </el-select>
@@ -30,7 +30,7 @@
                     <el-select v-model="plugin_query.category_id" @change="search_by_category()" clearable placeholder="应用归类">
                         <el-option v-for="item in category_options" :label="item.name" :key="item.id" :value="item.id"></el-option>
                     </el-select>
-                    </el-form-item>
+                    </el-form-item>-->
                     <el-form-item style="width:12%">
                         <el-button type="primary" icon="el-icon-search" @click="search()">查询</el-button>
                     </el-form-item>
@@ -38,7 +38,7 @@
             </el-col>
             <el-col :span="5"  style="text-align: right">
                 <el-button  icon="el-icon-refresh" @click="refresh()">刷新</el-button>
-                <el-button v-if="has_permission('exploit_plugin_add')" type="primary" icon="el-icon-plus" @click="handle_plugin_add">新建漏洞插件</el-button>
+                <el-button v-if="has_permission('exploit_plugin_add')" type="primary" icon="el-icon-plus" @click="handle_task_add">新建任务</el-button>
             </el-col>
         </el-row>
         <el-row></el-row>
@@ -95,73 +95,152 @@
         </div>
 
         <el-dialog :visible.sync="dialogVisible" :title="title" v-if="dialogVisible" width="80%"  :close-on-click-modal="false">
-            <el-tabs v-model="activeName" >
-                <el-tab-pane label="单条记录" name="first">
-                    <el-form :model="form" label-width="150px">
-                        <el-form-item label="漏洞插件名称" prop="name" required>
-                            <el-input v-model="form.name" placeholder="漏洞插件名称，例如：SharePoint(CVE-2019-0604) Rce 利用插件"></el-input>
-                        </el-form-item>
-                        <el-form-item label="漏洞编号" prop="cve" required>
-                            <el-input v-model="form.cve" placeholder="漏洞cve编号，例如：CVE-2019-0604"></el-input>
-                        </el-form-item>
-                        <el-form-item label="漏洞类型" prop="vul_type_id" required>
-                            <el-select v-model="form.vul_type_id" placeholder="所属漏洞类型">
-                                <el-option v-for="item in vultype_options" :label="item.name" :key="item.id" :value="item.id"></el-option>
-                            </el-select>
-                        </el-form-item>
-                        <el-form-item label="危害等级" prop="vullevel_id" required>
-                            <el-select v-model="form.vullevel_id" placeholder="危害等级">
-                                <el-option v-for="item in level_options" :label="item.name" :key="item.id" :value="item.id"></el-option>
-                            </el-select>
-                        </el-form-item>
-                        <el-form-item label="利用效果" prop="effect_id" required>
-                            <el-select v-model="form.effect_id" placeholder="利用效果">
-                                <el-option v-for="item in effect_options" :label="item.name" :key="item.id" :value="item.id"></el-option>
-                            </el-select>
-                        </el-form-item>
-                        <el-form-item label="应用名称" prop="application_id" required>
-                            <el-select v-model="form.application_id" placeholder="应用名称">
-                                <el-option v-for="item in application_options" :label="item.name" :key="item.id" :value="item.id"></el-option>
-                            </el-select>
-                        </el-form-item>
-                        <el-form-item label="应用归类" prop="category_id" required>
-                            <el-select v-model="form.category_id" placeholder="应用归类">
-                                <el-option v-for="item in category_options" :label="item.name" :key="item.id" :value="item.id"></el-option>
-                            </el-select>
-                        </el-form-item>
-                        <el-form-item label="目标版本" prop="affect_version">
-                            <el-input v-model="form.affect_version" placeholder="适用于该漏洞插件的应用版本，例如：Apache Solr 5.0.0 - 8.3.1"></el-input>
-                        </el-form-item>
-                        <el-form-item label="目标语言" prop="language_id" required>
-                            <el-select v-model="form.language_id" placeholder="目标语言">
-                                <el-option v-for="item in language_options" :label="item.name" :key="item.id" :value="item.id"></el-option>
-                            </el-select>
-                        </el-form-item>
-                        <el-form-item label="目标系统" prop="os" required>
-                            <el-input v-model="form.os" placeholder="适用于该漏洞插件的系统，例如：windows、linux"></el-input>
-                        </el-form-item>
-                      
-                        <el-form-item label="漏洞描述" prop="desc">
-                            <el-input v-model="form.desc" type="textarea" :autosize="{ minRows: 5}" placeholder="漏洞描述信息"></el-input>
-                        </el-form-item>
-                    </el-form>
-                </el-tab-pane>
+            <el-steps :active="active" process-status="process" finish-status="success">
+                <el-step title="任务" description="输入任务名称及描述">></el-step>
+                <el-step title="目标" description="输入目标url或上传目标文件"></el-step>
+                <el-step title="漏洞插件" description="选择漏洞插件">></el-step>
+                <el-step title="执行" description="选择任务是否立刻执行"></el-step>
+                <el-step title="完成" description="任务配置完成"></el-step>
+            </el-steps>
+            <el-divider></el-divider>
+            <el-form :model="form" label-width="150px">
+                <div v-show="active == 0">
+                    <el-form-item label="任务名称" prop="name" required>
+                        <el-input v-model="form.name" placeholder="任务名称"></el-input>
+                    </el-form-item>
+                    <el-form-item label="任务描述" prop="desc" required>
+                        <el-input v-model="form.desc" placeholder="任务描述"></el-input>
+                    </el-form-item>
+                </div>
+                <div v-show="active == 1">
+                    <el-form-item label="目标格式" required>
+                        <el-radio v-model="radio" label="1">列表</el-radio>
+                        <el-radio v-model="radio" label="2">文件</el-radio>
+                    </el-form-item>
+                    <el-form-item v-if="radio=='1'" label="目标列表" prop="url_list" required>
+                        <el-input v-model="form.url_list" type="textarea" :autosize="{ minRows: 5}" placeholder="http://127.0.0.1/
+http://127.0.0.1:8080/
+http://192.168.1.2:8080/
+                        "></el-input>
+                    </el-form-item>
+                    <el-form-item v-if="radio=='2'" label="目标文件" prop="url_file" required>
+                        <el-upload class="upload-demo" drag action="" multiple>
+                            <i class="el-icon-upload"></i>
+                            <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+                            <div class="el-upload__tip" slot="tip">只能上传txt文件，且不超过1M</div>
+                        </el-upload>
+                    </el-form-item>
+                </div>
+                <div v-show="active == 2">
+                    <el-row>
+                        <el-col :span="19">
+                            <el-form :inline="true" :model="plugin_query">
+                                <el-form-item>
+                                    <el-input v-model="plugin_query.name" clearable placeholder="漏洞插件名称"></el-input>
+                                </el-form-item>
+                                <el-form-item style="width:10%">
+                                <el-select v-model="plugin_query.vul_type_id" @change="search_by_vultype()" clearable placeholder="漏洞类型">
+                                    <el-option v-for="item in vultype_options" :label="item.name" :key="item.id" :value="item.id"></el-option>
+                                </el-select>
+                                </el-form-item>
+                                <el-form-item style="width:10%">
+                                <el-select v-model="plugin_query.level_id" @change="search_by_level()" clearable placeholder="危害等级">
+                                    <el-option v-for="item in level_options" :label="item.name" :key="item.id" :value="item.id"></el-option>
+                                </el-select>
+                                </el-form-item>
+                                <el-form-item style="width:10%">
+                                <el-select v-model="plugin_query.effect_id" @change="search_by_effect()" clearable placeholder="利用效果">
+                                    <el-option v-for="item in effect_options" :label="item.name" :key="item.id" :value="item.id"></el-option>
+                                </el-select>
+                                </el-form-item>
+                                <el-form-item style="width:10%">
+                                <el-select v-model="plugin_query.application_id" @change="search_by_application()" clearable placeholder="应用名称">
+                                    <el-option v-for="item in application_options" :label="item.name" :key="item.id" :value="item.id"></el-option>
+                                </el-select>
+                                </el-form-item>
+                                <el-form-item style="width:10%"> 
+                                <el-select v-model="plugin_query.category_id" @change="search_by_category()" clearable placeholder="应用归类">
+                                    <el-option v-for="item in category_options" :label="item.name" :key="item.id" :value="item.id"></el-option>
+                                </el-select>
+                                </el-form-item>
+                                <el-form-item style="width:10%">
+                                    <el-button type="primary" icon="el-icon-search" @click="search()">查询</el-button>
+                                </el-form-item>
+                            </el-form>
+                        </el-col>
+                        <el-col :span="5"  style="text-align: right">
+                            <el-button  icon="el-icon-refresh" @click="refresh()">刷新</el-button>
+                            <!--<el-button v-if="has_permission('exploit_plugin_add')" type="primary" icon="el-icon-plus" @click="handle_task_add">新建任务</el-button>-->
+                        </el-col>
+                    </el-row>
+                    <el-table ref="multipleTable" :data="plugins.data" highlight-current-row :row-key="getRowKeys" :expand-row-keys="expands" @expand-change="get_plugin_extend" v-loading="tableLoading" style="width: 100%; margin-top: 20px">
+                        <el-table-column type="selection">
+                        </el-table-column>
+                        <el-table-column type="expand">
+                            <template slot-scope="props">
+                                <el-form v-if="props.row.extend" label-position="left" inline class="demo-table-expand">
+                                    <el-form-item label="目标版本:"><span>{{ props.row.extend.affect_version }}</span></el-form-item>
+                                    <el-form-item label=""><span></span></el-form-item>
+                                    <el-form-item label="目标语言:"><span>{{ props.row.extend.language_name }}</span></el-form-item>
+                                    <el-form-item label=""><span></span></el-form-item>
+                                    <el-form-item label="目标系统:"><span>{{ props.row.extend.os }}</span></el-form-item>
+                                    <el-form-item label=""><span></span></el-form-item>
+                                    <el-form-item label="提交时间:"><span>{{ props.row.extend.enter_time }}</span></el-form-item>
+                                    <el-form-item label=""><span></span></el-form-item>
+                                    <el-form-item label="更新时间:"><span>{{ props.row.extend.update_time }}</span></el-form-item>
+                                    <el-form-item label=""><span></span></el-form-item>
+                                    <el-form-item label="漏洞描述:"><span>{{ props.row.extend.desc }}</span></el-form-item>
+                                </el-form>
+                            <!-- <el-row v-else style="text-align: center">
+                                    <span style="color: #99a9bf">暂没有额外信息</span>
+                                </el-row>-->
+                            </template>
+                        </el-table-column>
 
-                <!--<el-tab-pane label="批量导入" name="second" v-if="importStatus" v-loading="import_loading" element-loading-text="正在导入...">
-                    <a :href= "download_url" download="host.xls">批量导入模板下载.xls</a>
-                    <div class="el-upload__tip"></div>
-                    <el-upload ref="upload" action="" :http-request="import_submit" name="file" :multiple="false"
-                               :before-upload="beforeAvatarUpload" :file-list="fileList">
-                        <el-button size="small" type="primary" v-if="has_permission('assets_host_add')">点击批量导入</el-button>
-                        <div slot="tip" class="el-upload__tip">只能上传xls/xlsx文件</div>
-                    </el-upload>
-                </el-tab-pane>-->
-            </el-tabs>
+                        <el-table-column prop="exploit_name" label="漏洞名称"></el-table-column>
+                        <el-table-column prop="cve" label="漏洞编号"></el-table-column>
+                        <el-table-column prop="vultype_name" label="漏洞类型"></el-table-column>
+                        <el-table-column prop="level_name" label="危害等级"></el-table-column>
+                        <el-table-column prop="effect_name" label="利用效果"></el-table-column>
+                        <el-table-column prop="application_name" label="应用名称"></el-table-column>
+                        <el-table-column prop="category_name" label="应用归类"></el-table-column>
+                        <el-table-column prop="author" label="提交者"></el-table-column>
+                        <el-table-column label="操作" width="270px" v-if="has_permission('assets_host_edit|assets_host_del|assets_host_valid')">
+                            <template slot-scope="scope">
+                                <el-button v-if="has_permission('assets_host_edit')" size="small" icon="el-icon-edit" @click="handleEdit(scope.row)">编辑</el-button>
+                                <el-button v-if="has_permission('assets_host_del')" size="small" icon="el-icon-delete" type="danger" @click="deleteCommit(scope.row)"
+                                        :loading="btnDelLoading[scope.row.id]">删除
+                                </el-button>
+                            </template>
+                        </el-table-column>
+                    </el-table>
 
-            <div slot="footer">
+                    <!--分页-->
+                    <div class="pagination-bar" v-if="plugins.total > 10">
+                        <el-pagination
+                                @current-change="handleCurrentChange"
+                                :current-page="currentPage"  layout="total, prev, pager, next"
+                                :total="plugins.total">
+                        </el-pagination>
+                    </div>
+                </div>
+                <div v-show="active == 3">
+                    <el-form-item label="立刻执行任务"  required>
+                        <el-switch v-model="at_once" active-color="#13ce66"  active-text="启用" inactive-text="关闭"></el-switch>
+                    </el-form-item>
+                </div>
+                <div v-show="active == 4">
+                    
+                </div>
+            </el-form>
+            <el-button v-if="active > 0" type="primary" style="margin-top: 12px"  icon="el-icon-back" @click="pre">上一步</el-button>
+            <el-button v-if="active < 4" type="primary" style="margin-top: 12px" icon="el-icon-right" @click="next">下一步</el-button>
+            <el-button v-if="active >= 4" type="primary" @click="task_commit" :loading="btnSaveLoading">创建任务</el-button>
+            
+            <!--<div slot="footer">
                 <el-button @click="dialogVisible=false">取消</el-button>
-                <el-button type="primary" @click="plugin_commit" :loading="btnSaveLoading">保存</el-button>
-            </div>
+                <el-button type="primary" @click="task_commit" :loading="btnSaveLoading">创建</el-button>
+            </div>-->
         </el-dialog>
     </div>
 </template>
@@ -186,6 +265,9 @@
     export default {
         data () {
             return {
+                active: 0,
+                radio:"1",
+                at_once:false,
                 plugin_query: {
                     name: '',
                     vul_type_id: '',
@@ -208,13 +290,17 @@
                 application_options: [],
                 category_options: [],
                 language_options: [],
-                expands: [],
-                file_name: 'file',
-                title: '编辑主机',
-                activeName:'first'
+                expands: []
             }
         },
         methods: {
+            next() {
+                if (this.active++ > 4)this.active = 5
+                },
+                // 步骤条上一步的方法
+            pre() {
+                if (this.active-- < 0) this.active = 0
+                },
              //刷新
             refresh(){
                 this.search(this.currentPage);
@@ -321,9 +407,9 @@
                 }
             },
 
-            handle_plugin_add () {
+            handle_task_add () {
                 this.form = {};
-                this.title = '添加漏洞插件';
+                this.title = '创建任务';
                 this.dialogVisible = true;
                 //this.importStatus = true;
             },
@@ -336,13 +422,13 @@
                 this.title = '编辑漏洞插件';
                 //this.importStatus = false;
             },
-            plugin_commit () {
+            task_commit () {
                 this.btnSaveLoading = true;
                 let request;
                 if (this.form.id) {
-                    request = this.$http.put(`/api/exploit/plugins/${this.form.id}`, this.form)
+                    request = this.$http.put(`/api/task/index/${this.form.id}`, this.form)
                 } else {
-                    request = this.$http.post(`/api/exploit/plugins`, this.form)
+                    request = this.$http.post(`/api/task/index`, this.form)
                 }
                 request.then(() => {
                     this.dialogVisible = false;
